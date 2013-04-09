0afb653f87b153bb53bbf479f35ad35089161e91
Zorayr Khalapyan <zkhalapyan@gmail.com>
2013-04-09 06:15:37 -0700

    Implemented installed campaigns display according to the new architecture.

--- src/www/js/init/init.InstalledCampaigns.js
+++ src/www/js/init/init.InstalledCampaigns.js
@@ -1,15 +1,9 @@
-Init.invokeOnReady(function() {
+Init.invokeOnReady(function () {
+    "use strict";
 
-    console.log("init.InsalledCampaigns: Initializing.");
-    
-    var onSuccess = function(){
-        $('#campaigns').append(Campaigns.render(true));
-    };
-
-    var onError = function(){
-        MessageDialogController.showMessage("Unable to download campaigns. Please try again later.")
-    }
-
-    Campaigns.download(false, onSuccess, onError);
+    var pageModel = PageModel("installedCampaigns", "My Campaigns");
+    pageModel.setTopButton("Add Campaign", PageController.openAvailableCampaigns);
+    pageModel.setView(CampaignsController.getInstalledCampaignsView());
+    PageController.registerPage(pageModel);
 
 });
