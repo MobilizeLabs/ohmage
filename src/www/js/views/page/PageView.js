cb38a23a5de6923b86c49c451a93ba763ee3b6b2
Zorayr Khalapyan <zkhalapyan@gmail.com>
2013-04-09 07:19:28 -0700

    Integrated page title, and page home link.

--- src/www/js/views/page/PageView.js
+++ src/www/js/views/page/PageView.js
@@ -13,9 +13,13 @@ var PageView = function (pageModel) {
      */
     var header, view, footer;
 
+    var headerTitle = document.getElementById("header-title");
+    var homeLink = document.getElementById("header-home-link");
+
     that.render = function () {
         mwf.decorator.TopButton(pageModel.getTopButtonName(), null, pageModel.getTopButtonCallback(), true);
-        document.getElementById("header-title").innerHTML = pageModel.getPageTitle();
+        headerTitle.innerHTML = pageModel.getPageTitle();
+        TouchEnabledItemModel.bindTouchEvent(homeLink, homeLink, PageController.goToRootPage);
         return pageModel.getView().render();
     };
 
