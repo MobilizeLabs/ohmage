f4e3c46c207ea26e806159fa22459f79a1d5979f
Zorayr Khalapyan <zkhalapyan@gmail.com>
2013-04-15 06:31:13 -0700

    JSLinted MessageDialogController.js.

--- src/www/js/controllers/messaging/MessageDialogController.js
+++ src/www/js/controllers/messaging/MessageDialogController.js
@@ -1,7 +1,7 @@
 var MessageDialogController = (function () {
-    
+    "use strict";
     var that = {};
-    
+
     /**
      * Invokes the method 'fun' if it is a valid function. In case the function
      * method is null, or undefined then the error will be silently ignored.
@@ -9,18 +9,18 @@ var MessageDialogController = (function () {
      * @param fun  the name of the function to be invoked.
      * @param args the arguments to pass to the callback function.
      */
-    var invoke = function( fun, args ) {
-        if( fun && typeof fun === 'function' ) {
-            fun( args );
+    var invoke = function (fun, args) {
+        if (fun && typeof fun === 'function') {
+            fun(args);
         }
     };
-    
-    that.showMessage = function( message, callback, title, buttonName ) {
+
+    that.showMessage = function (message, callback, title, buttonName) {
 
         title = title || "ohmage";
         buttonName = buttonName || 'OK';
 
-        if( navigator.notification && navigator.notification.alert ) {
+        if (navigator.notification && navigator.notification.alert) {
 
             navigator.notification.alert(
                 message,    // message
@@ -31,13 +31,13 @@ var MessageDialogController = (function () {
 
         } else {
 
-            alert( message );
-            invoke( callback );
+            alert(message);
+            invoke(callback);
         }
 
     };
 
-    that.showConfirm = function( message, callback, buttonLabels, title ) {
+    that.showConfirm = function (message, callback, buttonLabels, title) {
 
         //Set default values if not specified by the user.
         buttonLabels = buttonLabels || 'OK,Cancel';
@@ -46,32 +46,29 @@ var MessageDialogController = (function () {
         title = title || "ohmage";
 
         //Use Cordova version of the confirm box if possible.
-        if(navigator.notification && navigator.notification.confirm){
-
-                var _callback = function(index){
-                    if( callback ) {
-
-                        if(DeviceDetection.isDeviceiOS())
-                            index = buttonList.length - index;
-
-                        callback( index == 1 );
+        if (navigator.notification && navigator.notification.confirm) {
+            var callbackWrapper = function (index) {
+                if (callback) {
+                    if (DeviceDetection.isDeviceiOS()) {
+                        index = buttonList.length - index;
                     }
-                };
+                    callback(index === 1);
+                }
+            };
 
-                navigator.notification.confirm(
-                    message,      // message
-                    _callback,    // callback
-                    title,        // title
-                    buttonLabels  // buttonName
-                );
+            navigator.notification.confirm(
+                message,      // message
+                callbackWrapper,    // callback
+                title,        // title
+                buttonLabels  // buttonName
+            );
 
         //Default to the usual JS confirm method.
         } else {
-            invoke( callback, confirm( message ) );
+            invoke(callback, confirm(message));
         }
-        
-    };
 
+    };
     return that;
-    
-})();
+
+}());
