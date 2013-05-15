57bc5a13c8551e180ece0e74615744c588383970
Zorayr Khalapyan <zkhalapyan@gmail.com>
2013-05-14 21:31:33 -0700

    Updated DeviceDetection.js to be JSLint compliant.

--- src/www/js/util/DeviceDetection.js
+++ src/www/js/util/DeviceDetection.js
@@ -6,51 +6,52 @@
  * @author Zorayr Khalapyan
  * @version 11/05/2012
  */
-var DeviceDetection = (function() {
-    var self = {};
+var DeviceDetection = (function () {
+    "use strict";
+    var that = {};
 
     var userAgent = navigator.userAgent;
 
     var documentURL = document.URL;
 
-    var matchUserAgent = function( agentRegexp ) {
+    var matchUserAgent = function (agentRegexp) {
         return userAgent.match(agentRegexp);
     };
 
     /**
      * Returns true if the user is on a mobile device.
      */
-    self.isOnDevice = function() {
+    that.isOnDevice = function () {
         return matchUserAgent(/(iPhone|iPod|iPad|Android|BlackBerry)/);
     };
 
     /**
      * Returns true if the user is currently on an iPhone, iPod, or an iPad.
      */
-    self.isDeviceiOS = function() {
-        return matchUserAgent( /(iPhone|iPod|iPad)/ );
+    that.isDeviceiOS = function () {
+        return matchUserAgent(/(iPhone|iPod|iPad)/);
     };
 
     /**
      * Returns true if the user is currently on an Android device.
      */
-    self.isDeviceAndroid = function() {
+    that.isDeviceAndroid = function () {
         return matchUserAgent(/(Android)/);
     };
 
     /**
      * Returns true if the current application is running on a Cordova build.
      */
-    self.isNativeApplication = function() {
-        return documentURL.indexOf( 'http://' ) === -1 &&
-               documentURL.indexOf( 'https://' ) === -1;
+    that.isNativeApplication = function () {
+        return documentURL.indexOf('http://') === -1 &&
+               documentURL.indexOf('https://') === -1;
     };
 
     /**
      * The method sets the current user agent string and can be used for agent
      * spoofing or for testing.
      */
-    self.setUserAgent = function( newUserAgent ) {
+    that.setUserAgent = function (newUserAgent) {
         userAgent = newUserAgent;
     };
 
@@ -58,10 +59,10 @@ var DeviceDetection = (function() {
      * The method sets the current document URL string and can be used for
      * testing native application detection.
      */
-    self.setDocumentURL = function( newDocumentURL ) {
+    that.setDocumentURL = function (newDocumentURL) {
         documentURL = newDocumentURL;
     };
 
-    return self;
+    return that;
 
 }());
